4334c0b48b3e77f54c3644993623ff0a673b29fd
Karsten Taylor <karstentaylor0@gmail.com>
2020-06-10 10:34:29 -0700

    added api.js

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student';
+
+const getItems = function () {
+  return fetch('${BASE_URL}/items');
+};
+
+export default {
+  getItems,
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import $ from 'jquery';
+import api from './api';
 
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
-
 const main = function () {
+  api
+    .getItems()
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 
   shoppingList.bindEventListeners();
   shoppingList.render();
