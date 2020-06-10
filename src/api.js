b9c125b203c4c125c40d92f35ad6338f5c06d25b
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-10 11:44:15 -0700

    step 6 complete

--- src/api.js
+++ src/api.js
@@ -1,9 +1,22 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student';
 
 const getItems = function () {
-  return fetch('${BASE_URL}/items');
+  return fetch(`${BASE_URL}/items`);
+};
+
+const createItem = function (name) {
+  const newItem = JSON.stringify({name});
+  
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
 };
 
 export default {
   getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import $ from 'jquery';
-import api from './api';
+import store from './store'
 
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
 import shoppingList from './shopping-list';
 
 const main = function () {
-  api
-    .getItems()
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+  api.getItems()
+
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
